365f974acbf995fa7e12cc733681e3f75d3e913a
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-11-20 14:28:18 +0100

    wip: Fiw docker

---
+++ servers/cash/src/main/java/com/cashmanager/cash/models/Client.java
@@ -0,0 +1,21 @@
+package com.cashmanager.cash.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+}
