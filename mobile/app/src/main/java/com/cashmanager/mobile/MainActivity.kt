66967d9899ce5f9304297dda7f62b47681b762da
TRAORE <traore@MacBook-Pro-de-TRAORE.local>
2023-12-20 07:31:41 +0100

    ADD: page login

--- mobile/app/src/main/java/com/cashmanager/mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/cashmanager/mobile/MainActivity.kt
@@ -2,12 +2,19 @@ package com.cashmanager.mobile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cashmanager.mobile.fragments.AuthenticationFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injection de notre page dans la boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, AuthenticationFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
 }
--- mobile/app/src/main/java/com/cashmanager/mobile/fragments/AuthenticationFragment.kt
+++ mobile/app/src/main/java/com/cashmanager/mobile/fragments/AuthenticationFragment.kt
@@ -1,4 +1,14 @@
 package com.cashmanager.mobile.fragments
 
-class AuthenticationFragment() {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cashmanager.mobile.R
+
+class AuthenticationFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_login, container, false)
+    }
 }
