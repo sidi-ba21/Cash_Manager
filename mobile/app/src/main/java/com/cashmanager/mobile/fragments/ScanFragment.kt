d500ed601e64524447622ef7854b1f0ae2a437f7
TRAORE <traore@MacBook-Pro-de-TRAORE.local>
2024-01-07 16:17:09 +0100

    ADD: navigation and page scan

---
+++ mobile/app/src/main/java/com/cashmanager/mobile/fragments/ScanFragment.kt
@@ -0,0 +1,23 @@
+package com.cashmanager.mobile.fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.cashmanager.mobile.R
+import com.cashmanager.mobile.adapter.ListAdapter
+
+class ScanFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view =  inflater?.inflate(R.layout.fragment_scan, container, false)
+
+        val verticalRecyclerView = view?.findViewById<RecyclerView>(R.id.vertical_recycler_view)
+        verticalRecyclerView?.adapter = ListAdapter(R.layout.item_vertical_shopping)
+
+        return view
+    }
+
+}
