990754ba69e8713ade1e81e3cad6d9bdb5089013
TRAORE <traore@MacBook-Pro-de-TRAORE.local>
2024-01-10 10:15:14 +0100

    ADD: implementation login

--- mobile/app/src/main/java/com/cashmanager/mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/cashmanager/mobile/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.cashmanager.mobile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -9,13 +11,39 @@ import com.cashmanager.mobile.fragments.ScanFragment
 import com.cashmanager.mobile.fragments.ShoppingFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadFragment(ScanFragment())
+
+        sharedPreferences = getSharedPreferences("login_info", Context.MODE_PRIVATE)
+
+        // Vérification si la clé "isLoggedIn" est définie
+        if (sharedPreferences.contains("isLoggedIn")) {
+             //Si la clé n'existe pas, initialisation à false
+            with(sharedPreferences.edit()) {
+//                putBoolean("isLoggedIn", false)
+//                apply()
+            }
+        }
+
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        if (isLoggedIn) {
+            showLoggedInView()
+        } else {
+            loadFragment(AuthenticationFragment())
+        }
+
+    }
+
+    private fun showLoggedInView() {
         val navigationBottonView = findViewById<BottomNavigationView>(R.id.navigation_view)
         navigationBottonView.setOnItemSelectedListener {
             when (it.itemId) {
@@ -35,12 +63,21 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+        loadFragment(ScanFragment())
     }
-    private fun loadFragment(frangment: Fragment) {
+
+     private fun loadFragment(frangment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, frangment)
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    fun updateLoginStatus(status: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isLoggedIn", status)
+        editor.apply()
+        recreate()
     }
 
 }
--- mobile/app/src/main/java/com/cashmanager/mobile/fragments/AuthenticationFragment.kt
+++ mobile/app/src/main/java/com/cashmanager/mobile/fragments/AuthenticationFragment.kt
@@ -6,32 +6,72 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.cashmanager.mobile.R
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.cashmanager.mobile.fragments.ScanFragment
+import com.android.volley.toolbox.StringRequest;
+import com.cashmanager.mobile.MainActivity
+import org.json.JSONObject
 
 class AuthenticationFragment: Fragment() {
+
+    private val BASE_URL = "http://10.0.2.2:8080/api/v1/auth/login"
+    //private val BASE_URL = "http://10.0.2.2:8080/api/v1/clients"
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
 
         val emailEditText = view?.findViewById<EditText>(R.id.name_email_login)
         val passwordEditText = view?.findViewById<EditText>(R.id.name_password_login)
         val loginButton = view?.findViewById<Button>(R.id.name_button_login)
-
         // Ajouter un Listener au bouton de connexion
         loginButton?.setOnClickListener {
             val email = emailEditText?.text.toString()
             val password = passwordEditText?.text.toString()
-            authenticateUser(email, password)
-        }
 
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                authenticateUser(email, password)
+            } else {
+                Toast.makeText(context, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
+            }
+        }
         return view
     }
 
     private fun authenticateUser(email: String, password: String) {
-        // Appel à l'API pour authentifier l'utilisateur
-        // Utilisez Retrofit ou une autre bibliothèque pour envoyer les données au serveur
-        // Gérez les réponses et les erreurs ici
-        // Assurez-vous de gérer les appels réseau de manière asynchrone (par exemple, avec des coroutines)
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        val params = JSONObject()
+        params.put("email", email)
+        params.put("password", password)
+        val stringRequest = object : StringRequest(
+            Method.POST, BASE_URL,
+            Response.Listener { response ->
+                if (response.isNotEmpty()) {
+                    (activity as? MainActivity)?.updateLoginStatus(true)
+                }
+            },
+            Response.ErrorListener { error ->
+                println(error)
+                Toast.makeText(context, "Email ou mot de passe incorrect", Toast.LENGTH_SHORT).show()
+            }) {
+            override fun getBodyContentType(): String = "application/json"
+
+            override fun getBody(): ByteArray {
+                val jsonObject = JSONObject().apply {
+                    put("email", email)
+                    put("password", password)
+                }
+                return jsonObject.toString().toByteArray()
+            }
+        }
+        requestQueue.add(stringRequest)
     }
 
 }
+
