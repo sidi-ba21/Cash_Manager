f4b7b32cc0019e84b38b63bd32009e34aa438142
TRAORE <traore@MacBook-Pro-de-TRAORE.local>
2024-01-12 20:09:20 +0100

    ADD: active scan

--- mobile/app/src/main/java/com/cashmanager/mobile/fragments/ScanFragment.kt
+++ mobile/app/src/main/java/com/cashmanager/mobile/fragments/ScanFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -20,9 +21,7 @@ import com.google.zxing.integration.android.IntentResult
 
 class ScanFragment : Fragment() {
 
-    companion object {
-        private const val PERMISSION_REQUEST_CAMERA = 1
-    }
+
 
     // Appelé lorsque la vue du fragment est créée
     override fun onCreateView(
@@ -30,10 +29,20 @@ class ScanFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_scan, container, false)
 
-        val topLinearLayout: LinearLayout = view.findViewById(R.id.topLinearLayoutScan)
+        val imageItemScan: ImageView = view.findViewById(R.id.image_item_scan)
+        imageItemScan.setOnClickListener {
+            val intentIntegrator = IntentIntegrator(activity)
+            intentIntegrator.setOrientationLocked(true)
+            intentIntegrator.setPrompt("Scan a QR code")
+            intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            intentIntegrator.initiateScan()
+        }
+
+
 
         val verticalRecyclerView = view?.findViewById<RecyclerView>(R.id.vertical_recycler_view)
 
@@ -42,79 +51,15 @@ class ScanFragment : Fragment() {
         return view
     }
 
-    // Fonction pour configurer le scanner QR code
-//    private fun setupQRCodeScanner() {
-//        // Vérification de la permission de la caméra
-//        if (ContextCompat.checkSelfPermission(
-//                requireContext(),
-//                Manifest.permission.CAMERA
-//            ) == PackageManager.PERMISSION_GRANTED
-//        ) {
-//            // Initialisation du scanner QR code
-//            initQRCodeScanner()
-//        } else {
-//            // Demande de permission de la caméra si elle n'est pas accordée
-//            ActivityCompat.requestPermissions(
-//                requireActivity(),
-//                arrayOf(Manifest.permission.CAMERA),
-//                PERMISSION_REQUEST_CAMERA
-//            )
-//        }
-//    }
-
-    // Fonction pour initialiser le scanner QR code
-    //private fun initQRCodeScanner() {
-        // Initialisation du scanner QR code ici
-        // Appel de la fonction pour démarrer le scanning automatiquement
-       // startScanning()
-    //}
-
-    // Fonction pour démarrer le scanning automatiquement
-//    private fun startScanning() {
-//        // Utilisation de ZXing pour démarrer le scanning
-//        IntentIntegrator.forSupportFragment(this)
-//            .setPrompt("") // Vous pouvez laisser le prompt vide pour un démarrage automatique
-//            .initiateScan()
-//    }
-
-    // Gestionnaire pour la réponse de la demande de permission de la caméra
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        if (requestCode == PERMISSION_REQUEST_CAMERA) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                // La permission de la caméra a été accordée, initialisation du scanner QR code
-//                initQRCodeScanner()
-//            } else {
-//                // La permission de la caméra n'a pas été accordée, affichage d'un message et fin de l'activité
-//                Toast.makeText(
-//                    requireContext(),
-//                    "La permission de la caméra est requise",
-//                    Toast.LENGTH_LONG
-//                ).show()
-//                activity?.finish()
-//            }
-//        }
-//    }
-
-    // Gestionnaire pour le résultat du scanning QR code
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        // Récupération du résultat du scanning avec ZXing
-//        val result: IntentResult =
-//            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-//        if (result.contents != null) {
-//            // Traitement du résultat du scanning (ici, simplement affichage dans un Toast)
-//            Toast.makeText(requireContext(), "Scanned: ${result.contents}", Toast.LENGTH_LONG)
-//                .show()
-//
-//            // Vous pouvez ajouter un traitement supplémentaire ici après le scanning
-//
-//            // Redémarrage du scanning automatiquement
-//            startScanning()
-//        }
-//    }
+    @Deprecated("Explanation of why this method is deprecated")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // Récupération du résultat du scanning avec ZXing
+        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            val contents: String? = result.contents
+            if (contents != null) {
+                println(contents)
+            }
+        }
+    }
 }
